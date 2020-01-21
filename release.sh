4a73e929eb50a71af68228544eb1c8f41632432e
Jaakko Julin <jaakko.a.julin@jyu.fi>
2020-01-21 13:38:46 +0200

    Fixed release numbering script.

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ do_release() {
     git clean -f;
     rm -rf autom4te.cache/;
     git status;
-    version=$(grep "^AC_INIT" < configure.ac|sed "s/.*\[.*\].*\[\(.*\)\].*\[.*\].*/\1/");
+    version=$(grep "^AC_INIT" < configure.ac|sed "s/AC_INIT(\[.*\].*\[\(.*\)\].*\[.*\].*/\1/");
     echo "CHECK THE STATUS OF THE REPOSITORY."
     read -p "About to tag the repo and create a bare (autogen.sh needs to be run) source package for version $version. Is this ok? ^C if not.";
     git tag $version
